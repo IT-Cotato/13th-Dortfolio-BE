11d813e2b22a8f4916fa18185b3c3c86ca8c3a8c
syeon111 <psooy19@gmail.com>
2026-07-10 13:29:20 +0900

    feat: Activity, ActivityType Repository 추가

---
+++ spring/src/main/java/com/itcotato/dortfolio/activity/repository/ActivityTypeRepository.java
@@ -0,0 +1,11 @@
+package com.itcotato.dortfolio.activity.repository;
+
+import com.itcotato.dortfolio.activity.entity.ActivityType;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityTypeRepository extends JpaRepository<ActivityType, UUID> {
+
+    List<ActivityType> findAllByUserId(UUID userId);
+}
