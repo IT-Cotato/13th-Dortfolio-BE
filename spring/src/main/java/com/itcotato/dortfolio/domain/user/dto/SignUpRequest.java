e41d20827916d051c933c8dcbd448c7b9ebd5911
pearseona <baesuna2930@gmail.com>
2026-08-09 17:22:21 +0900

    fix: 회원가입 이용약관 동의 필드 재제거

--- spring/src/main/java/com/itcotato/dortfolio/domain/user/dto/SignUpRequest.java
+++ spring/src/main/java/com/itcotato/dortfolio/domain/user/dto/SignUpRequest.java
@@ -31,10 +31,6 @@ public record SignUpRequest(
         String nickname,
 
         // 약관 동의
-        @NotNull(message = "이용약관 동의 여부가 누락되었습니다.")
-        @AssertTrue(message = "이용약관 동의는 필수입니다.")
-        Boolean isTermsAgreed,
-
         @NotNull(message = "개인정보 수집 및 이용 동의 여부가 누락되었습니다.")
         @AssertTrue(message = "개인정보 수집 및 이용 동의는 필수입니다.")
         Boolean isPrivacyAgreed,
