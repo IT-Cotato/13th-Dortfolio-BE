1fc1cb71ef03e0c09d204dc64a73bcc5be704b6f
pearseona <baesuna2930@gmail.com>
2026-08-18 22:16:47 +0900

    feat: 에러 응답에 식별 코드 추가

--- spring/src/main/java/com/itcotato/dortfolio/global/response/ApiResponse.java
+++ spring/src/main/java/com/itcotato/dortfolio/global/response/ApiResponse.java
@@ -5,27 +5,33 @@ import lombok.Getter;
 
 @Getter
 public class ApiResponse<T> {
+
     private final boolean success;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final String code;
+
     private final String message;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T data;
 
-    private ApiResponse(boolean success, String message, T data) {
+    private ApiResponse(boolean success, String code, String message, T data) {
         this.success = success;
+        this.code = code;
         this.message = message;
         this.data = data;
     }
 
     public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<>(true, message, null);
+        return new ApiResponse<>(true, null, message, null);
     }
 
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(true, message, data);
+        return new ApiResponse<>(true, null, message, data);
     }
 
-    public static ApiResponse<Void> fail(String message) {
-        return new ApiResponse<>(false, message, null);
+    public static ApiResponse<Void> fail(String code, String message) {
+        return new ApiResponse<>(false, code, message, null);
     }
 }
